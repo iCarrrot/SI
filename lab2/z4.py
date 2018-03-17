b13f4aa3f9534939aaa0b012924ee64298c0a73b
iCarrrot <michal.mar3@gmail.com>
2018-03-17 12:32:52 +0100

    wersja 6/11, ale szybsza

--- lab2/z4.py
+++ lab2/z4.py
@@ -3,10 +3,11 @@ from random import randint
 board = []
 dirValue = {'U': (0, 1), 'D': (0, -1), 'R': (-1, 0), 'L': (1, 0)}
 meanMoves = {'S':'DURL', 'D':'DRL', 'U': 'URL', 'R':'DUR', 'L':'DUL'} 
-startSet = set()
-endSet = set()
+startTuple = ()
+endDict = dict()
 
-def prepareData(startSet, endSet, board):
+
+def prepareData(startTuple, endDict, board):
     with open("/home/michal/SI/lab2/zad_input.txt") as f:
         for line in f:
             board.append(line[:-1])
@@ -14,15 +15,17 @@ def prepareData(startSet, endSet, board):
     for row in board:
         _x = 0
         for l in row:
+            
             if l == 'S':
-                startSet.add((_x, _y))
+                startTuple+=((_x, _y),)
             elif l == 'G':
-                endSet.add((_x, _y))
+                endDict[(_x, _y)] =1
             elif l == 'B':
-                endSet.add((_x, _y))
-                startSet.add((_x, _y))
+                endDict[(_x, _y)] =1
+                startTuple+=((_x, _y),)
             _x += 1
         _y += 1
+    return startTuple
 
 
 def move(pos, _dir):
@@ -30,54 +33,64 @@ def move(pos, _dir):
     x1, y1 = dirValue[_dir]
     if board[y-y1][x-x1] != '#':
         (x, y) = (x-x1, y-y1)
-    if (x, y) in endSet:
+    if (x, y) in endDict:
         return (x, y), 1
     return (x, y), 0
 
 
 def moves(posSet, _dir):
     onGoal = 0
-    posSet = list(posSet)
-    for i in range(len(posSet)):
-        posSet[i], _inG = move(posSet[i], _dir)
+    tempList = []
+    # print("p", posSet)
+    for i in posSet:
+        # print(i)
+        t, _inG = move(i, _dir)
+        # print(t)
         onGoal += _inG
+        tempList+=[t]
+
     if onGoal == len(posSet):
-        return set(posSet), 1
-    return set(posSet), 0
+        return tuple(set(sorted(tempList))), 1
+    return tuple(set(sorted(tempList))), 0
 
 
-def faseOne(startSet):
+def faseOne(startTuple):
     final = ""
-    mSet = "UDRL"
+    # print(startTuple)
+    
+    # startTuple, _ = moves(startTuple, 'R')
+    
+    # mSet = "UDRL"
     for _ in range(len(board)-3):
-        startSet, _ = moves(startSet, 'U')
+        startTuple, _ = moves(startTuple, 'U')
         final+='U'
     for _ in range(len(board[0])-3):
-        startSet, _ = moves(startSet, 'R')
+        startTuple, _ = moves(startTuple, 'R')
         final+='R'
     for _ in range(len(board)):
-        startSet, _ = moves(startSet, 'U')
+        startTuple, _ = moves(startTuple, 'U')
         final+='U'
+    # print(len(startTuple))
     # for _ in range(len(board)*2):
     #     a = randint(0,3)
     #     c = mSet[a]
-    #     startSet, _ = moves(startSet, c)
+    #     startTuple, _ = moves(startTuple, c)
     #     final+=c
-    return startSet, final
+    return startTuple, final
 
 
-def faseOneAndHalf(startSet):
+def faseOneAndHalf(startTuple):
     visited = []
-    visited+=[startSet]
+    visited+=[startTuple]
     queue = deque()
-    queue.append((startSet,'S'))
+    queue.append((startTuple,'S'))
     onGoal = 0
     length = 0
     while(not onGoal):
         (posSet, oldM) = queue[0]
         if(len(oldM)>length):
             length+=1
-            print(length)
+            # print(length)
         # print (posSet)
         # print (oldM)
         other= []
@@ -105,15 +118,17 @@ def faseOneAndHalf(startSet):
 
 
 
-def faseTwo(startSet):
+def faseTwo(startTuple):
     visited = dict()
-    visited[tuple(sorted(startSet))] = 1
+    visited[startTuple] = 1
     queue = deque()
-    queue.append((startSet,'S'))
+    queue.append((startTuple,'S'))
     onGoal = 0
     length = 0
+    # print(queue)
     while(not onGoal):
         (posSet, oldM) = queue[0]
+        # print(oldM)
         # print(visited)
         if(len(oldM)>length):
             length+=1
@@ -124,9 +139,9 @@ def faseTwo(startSet):
             newPos, onGoal = moves(posSet, m)
             if onGoal:
                 return oldM[1:]+m
-            if tuple(sorted(newPos)) not in visited:
+            if newPos not in visited:
                 queue.append((newPos,oldM+m))
-                visited[tuple(sorted(newPos))] = 1
+                visited[newPos] = 1
         queue.popleft()
     return oldM[1:]
 
@@ -135,19 +150,19 @@ def faseTwo(startSet):
 
 def solver():
     final = ''
-    startSet = set()
-    prepareData(startSet, endSet, board)
-    # print(list(startSet))
-    startSet, _final = faseOne(startSet)
+    startTuple = ()
+    startTuple = prepareData(startTuple, endDict, board)
+    # print(list(startTuple))
+    startTuple, _final = faseOne(startTuple)
     final+=_final
     print(final)
 
-    # startSet, _final = faseOneAndHalf(startSet)
+    # startTuple, _final = faseOneAndHalf(startTuple)
     # final+=_final
     # print(final)
-    print(list(startSet))
+    # print(list(startTuple))
     
-    final += faseTwo(startSet)
+    final += faseTwo(startTuple)
     # final+="DRRRRDRRRRRRURRD"
     file = open("zad_output.txt", "w")
     file.write(final)
